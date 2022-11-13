3e1f75afa15068a2629c7f34f6d5403874bdb651
hasnain2025 <mehadi.2025@gmail.com>
2022-11-13 19:08:39 +0530

    added google login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,37 @@
 import chalk from "chalk";
+import passport from "passport";
 
+export const googlePrivider = () => {
+  try {
+    console.log("data-1", new Date());
+    passport.authenticate("google", { scope: ["profile"] });
+    console.log("data-2", new Date());
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const googleRedirect = async (req, res) => {
+  try {
+    console.log("req.body", req.body);
+    res.status(200).send({
+      success: true,
+      msg: "success",
+      data: {},
+    });
+  } catch (error) {
+    console.log(chalk.bgRed.bold(error?.message));
+    res.status(200).send(error?.message);
+  }
+};
+
+export const facebookPrivider = () => {
+  try {
+    passport.authenticate("facebook");
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 export const facebook = async (req, res) => {
   try {
     console.log("req.body", req.body);
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { facebook } from "../controllers/userController.js";
-
+import passport from "passport";
+import { googleRedirect } from "../controllers/userController.js";
 const router = express.Router();
 
-router.post("/facebook", facebook);
+// google auth route
+router.get(
+  "/google/provider",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+router.get(
+  "/google/redirect",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
+//google facebook route
+router.get("/facebook/provider", googleRedirect);
+router.get("/facebook/redirect", googleRedirect);
 
 export default router;
